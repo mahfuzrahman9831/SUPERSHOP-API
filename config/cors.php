2699b8c34d54009b83537bad344009db814c6e1b
tahazzat.dev@gmail.com <admin@mdabdurrazzak.com>
2026-06-18 15:05:37 +0600

    phase 19 testing complete

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,18 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+
+    'allowed_origins' => ['*'], 
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
 ];
